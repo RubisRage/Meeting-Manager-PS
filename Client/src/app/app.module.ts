9f94de04fd1283b4c98366fbea0e4d268fb38ec1
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2022-03-28 14:22:09 +0100

    Fixed conflict with importing modules more than once

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
