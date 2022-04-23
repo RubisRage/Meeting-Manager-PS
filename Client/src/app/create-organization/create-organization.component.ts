5608ef2405f6c49df20ef0c0d81e62f7302a635d
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2022-04-23 19:51:14 +0100

    Added requests to backend on submit

--- Client/src/app/create-organization/create-organization.component.ts
+++ Client/src/app/create-organization/create-organization.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {MatDialogRef} from "@angular/material/dialog";
 import {HttpHelperService} from "../services/http-helper.service";
+import {environment} from "../../environments/environment";
 
 @Component({
   templateUrl: './create-organization.component.html',
@@ -8,11 +9,40 @@ import {HttpHelperService} from "../services/http-helper.service";
 })
 export class CreateOrganizationComponent {
 
-  imgURL = "assets/pictures/org_default_picture.svg"
+  model = {
+    name: "",
+    description: "",
+    imgURL: "assets/pictures/org_default_picture.svg"
+  }
 
   constructor(public dialogRef: MatDialogRef<CreateOrganizationComponent>,
               private http: HttpHelperService) { }
 
+  onSubmit() {
 
+    this.http.post(`${environment.backend}/organizations`, this.model)
+      .subscribe({
+        next: () => {
+          this.dialogRef.close();
+        },
+        error: err => {
+          console.log(err);
+        }
+      })
+  }
+
+  onSelectedFile(event: Event) {
+    if(event.target && (event.target! as HTMLInputElement).files) {
+      const file = (event.target! as HTMLInputElement).files![0];
+
+      this.http.post(`${environment.backend}/images`, {
+        img: file
+      }).subscribe({
+        next: response => {
+          this.model.imgURL = response.imgURL;
+        }
+      });
+    }
+  }
 
 }
