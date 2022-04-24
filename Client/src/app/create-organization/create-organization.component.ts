79f5acd1b8d2c50c99398fb21a90965cddbafaa5
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2022-04-24 19:42:04 +0100

    Included form validation

--- Client/src/app/create-organization/create-organization.component.ts
+++ Client/src/app/create-organization/create-organization.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatDialogRef} from "@angular/material/dialog";
 import {HttpHelperService} from "../services/http-helper.service";
 import {environment} from "../../environments/environment";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   templateUrl: './create-organization.component.html',
@@ -15,17 +16,18 @@ export class CreateOrganizationComponent {
     imgURL: "assets/pictures/org_default_picture.svg"
   }
 
+  missingName = false;
+
   constructor(public dialogRef: MatDialogRef<CreateOrganizationComponent>,
               private http: HttpHelperService) { }
 
   onSubmit() {
-
     this.http.post(`${environment.backend}/organizations`, this.model)
       .subscribe({
         next: () => {
           this.dialogRef.close();
         },
-        error: err => {
+        error: (err: HttpErrorResponse) => {
           console.log(err);
         }
       })
@@ -45,4 +47,15 @@ export class CreateOrganizationComponent {
     }
   }
 
+  getErrorMessage() {
+    if(this.model.name.length < 5) {
+      return "¡El nombre de usuario es demasiado corto!"
+    }
+
+    if(this.model.name.length > 15) {
+      return "¡El nombre de usuario es demasiado largo!"
+    }
+
+    return "¡El nombre de usuario no es válido!"
+  }
 }
