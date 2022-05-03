68609e6a6eb2d752475ed0078eda537ca2439ddb
Illera1999 <joseill1414@gmail.com>
2022-05-03 11:36:29 +0100

    refresh-the-arquitecture

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
-import { MainOrganizationComponent } from './organization/main-organization/main-organization.component';
+import { MainOrganizationComponent } from './main/main-organization/main-organization.component';
 import { SignUpComponent } from './signup-form/signup-form.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component'
 import { HeaderDisplayComponent } from './organization/header-display/header-display.component';
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { CreateOrganizationComponent } from './create-organization/create-organi
 import {EditProfileComponent} from "./edit-profile/edit-profile.component";
 import { OrgDescriptionComponent } from './organization/org-description/org-description.component';
 import { MembersComponent } from './organization/members/members.component';
-import { MainOrganizationComponent } from './organization/main-organization/main-organization.component';
+import { MainOrganizationComponent } from './main/main-organization/main-organization.component';
 import { HeaderDisplayComponent } from './organization/header-display/header-display.component';
 import { OrganizationDisplayComponent } from './organization/header-display/organization-display/organization-display.component';
 import { ProfileDisplayComponent } from './organization/header-display/profile-display/profile-display.component';
@@ -21,6 +21,7 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatDialogModule } from "@angular/material/dialog";
 import { PreOrgScreenComponent } from './pre-org-screen/pre-org-screen.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
@@ -38,6 +39,7 @@ import { ErrorPageComponent } from './error-page/error-page.component';
     ProfileDisplayComponent,
     PreOrgScreenComponent,
     ErrorPageComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
