e339078c04a57aed816fe0f78b3a8380ad0b4137
Illera1999 <joseill1414@gmail.com>
2022-05-10 00:55:01 +0100

    try to compare the Arrays og Organizations

--- Client/src/app/main/pre-org-screen/pre-org-screen.component.ts
+++ Client/src/app/main/pre-org-screen/pre-org-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { AuthHelperService } from 'src/app/services/auth-helper.service';
 import { environment } from 'src/environments/environment';
 import { HttpHelperService } from '../../services/http-helper.service';
@@ -11,28 +11,33 @@ import { HttpHelperService } from '../../services/http-helper.service';
 export class PreOrgScreenComponent implements OnInit {
 
   organizations!: {id:number, name:string}[];
+  seguro!: {id:number, name:string}[];
 
   constructor(private http: HttpHelperService,
-    private authService:AuthHelperService) { }
+    private authService:AuthHelperService, private ref: ChangeDetectorRef) { }
 
   ngOnInit(): void {
-    /*
-    this.organizations = [
-      {id:1, name:"Padel"},
-      {id:2, name:"Boxeo"},
-      {id:3, name:"Produccion de Software"},
-      {id:4, name:"Clase"},
-    ];
-    */
     
+    setInterval( () => {
     this.http.get(environment.backend + "/users/" + this.authService.user?.username + "/organizations")
       .subscribe(
         (data) => {
-          this.organizations = data;
+//          if(this.equeals(this.seguro,data)){
+            this.organizations = data;
+  //        }
         }
-      );
+      );},1000);
+      this.organizations = this.seguro;
   }
 
-  
-
+/*   equeals(e1:{id:number, name:string}[], p2: any): Boolean{
+    if (e1.length != p2.length) return false;
+    
+    for (var i = 0; i < p2.length; i++) {
+      if (e1[i].id !== p2[i].id && e1[i].name === p2[i].name){
+        return false;
+      }
+    }  
+    return true;
+  };  */   
 }
